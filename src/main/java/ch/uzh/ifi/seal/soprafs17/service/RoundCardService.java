2a8d165f5d3efe4a9a54f0e2f8f44d940957c85c
meck93 <moritz.eck@gmail.com>
2017-03-30 20:28:16 +0200

    Improved RoundCardCreation

--- src/main/java/ch/uzh/ifi/seal/soprafs17/service/RoundCardService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs17/service/RoundCardService.java
@@ -36,13 +36,20 @@ public class RoundCardService {
      * @param gameId
      */
     public void createRoundCards(int amountOfPlayers, Long gameId) {
-
-        if (amountOfPlayers == 2){
-            createSevenTwoHeadCards(gameId);
+        // decide which card set to create
+        switch (amountOfPlayers) {
+            case 2:
+                createRoundCardSet(gameId, RoundCardType.TWO_HEADS);
+                break;
+            case 3:
+                createRoundCardSet(gameId, RoundCardType.THREE_HEADS);
+                break;
+            case 4:
+                createRoundCardSet(gameId, RoundCardType.FOUR_HEADS);
+                break;
         }
-        //if (amountOfPlayers == 3){createSevenThreeHeadCards();}
-        //if (amountOfPlayers == 4){createSevenFourHeadCards();}
     }
+
 
     /**
      * Creates a deck of seven roundcards with two heads.
@@ -50,88 +57,31 @@ public class RoundCardService {
      * @pre game =/= NULL
      * @post seven roundcards are stored under the game id
      */
-    public void createSevenTwoHeadCards(Long gameId){
+    public void createRoundCardSet(Long gameId, RoundCardType roundCardType){
+        createRoundCard(gameId, roundCardType, ShipSize.XL, ShipSize.L, ShipSize.M, ShipSize.M);
+        createRoundCard(gameId, roundCardType, ShipSize.XL, ShipSize.L, ShipSize.L, ShipSize.S);
+        createRoundCard(gameId, roundCardType, ShipSize.XL, ShipSize.M, ShipSize.M, ShipSize.S);
+        createRoundCard(gameId, roundCardType, ShipSize.XL, ShipSize.L, ShipSize.M, ShipSize.S);
+        createRoundCard(gameId, roundCardType, ShipSize.L, ShipSize.M, ShipSize.M, ShipSize.S);
+        createRoundCard(gameId, roundCardType, ShipSize.L, ShipSize.L, ShipSize.M, ShipSize.M);
+        createRoundCard(gameId, roundCardType, ShipSize.L, ShipSize.L, ShipSize.M, ShipSize.S);
+    }
 
-        ArrayList<ShipSize> shipsR1 = new ArrayList<>();
-        RoundCard roundCard1 = new RoundCard();
-        roundCard1.setGameId(gameId);
-        roundCard1.setHeads(RoundCardType.TWO_HEADS);
-        shipsR1.add(ShipSize.XL);
-        shipsR1.add(ShipSize.L);
-        shipsR1.add(ShipSize.M);
-        shipsR1.add(ShipSize.M);
-        roundCard1.setShipSizes(shipsR1);
-        roundCardRepository.save(roundCard1);
+    public void createRoundCard(Long gameId, RoundCardType roundCardType, ShipSize sizeOne, ShipSize sizeTwo, ShipSize sizeThree, ShipSize sizeFour) {
 
-        ArrayList<ShipSize> shipsR2 = new ArrayList<>();
-        RoundCard roundCard2 = new RoundCard();
-        roundCard2.setGameId(gameId);
-        roundCard2.setHeads(RoundCardType.TWO_HEADS);
-        shipsR2.add(ShipSize.XL);
-        shipsR2.add(ShipSize.L);
-        shipsR2.add(ShipSize.L);
-        shipsR2.add(ShipSize.S);
-        roundCard2.setShipSizes(shipsR2);
-        roundCardRepository.save(roundCard2);
+        RoundCard roundCard = new RoundCard();
+        roundCard.setGameId(gameId);
+        roundCard.setHeads(roundCardType);
 
-        ArrayList<ShipSize> shipsR3 = new ArrayList<>();
-        RoundCard roundCard3 = new RoundCard();
-        roundCard3.setGameId(gameId);
-        roundCard3.setHeads(RoundCardType.TWO_HEADS);
-        shipsR3.add(ShipSize.XL);
-        shipsR3.add(ShipSize.M);
-        shipsR3.add(ShipSize.M);
-        shipsR3.add(ShipSize.S);
-        roundCard3.setShipSizes(shipsR3);
-        roundCardRepository.save(roundCard3);
+        ArrayList<ShipSize> shipSizes = new ArrayList<>();
+        shipSizes.add(sizeOne);
+        shipSizes.add(sizeTwo);
+        shipSizes.add(sizeThree);
+        shipSizes.add(sizeFour);
+        roundCard.setShipSizes(shipSizes);
 
-        ArrayList<ShipSize> shipsR4 = new ArrayList<>();
-        RoundCard roundCard4 = new RoundCard();
-        roundCard4.setGameId(gameId);
-        roundCard4.setHeads(RoundCardType.TWO_HEADS);
-        shipsR4.add(ShipSize.L);
-        shipsR4.add(ShipSize.M);
-        shipsR4.add(ShipSize.M);
-        shipsR4.add(ShipSize.S);
-        roundCard4.setShipSizes(shipsR4);
-        roundCardRepository.save(roundCard4);
-
-        ArrayList<ShipSize> shipsR5 = new ArrayList<>();
-        RoundCard roundCard5 = new RoundCard();
-        roundCard5.setGameId(gameId);
-        roundCard5.setHeads(RoundCardType.TWO_HEADS);
-        shipsR5.add(ShipSize.L);
-        shipsR5.add(ShipSize.L);
-        shipsR5.add(ShipSize.M);
-        shipsR5.add(ShipSize.M);
-        roundCard5.setShipSizes(shipsR5);
-        roundCardRepository.save(roundCard5);
-
-        ArrayList<ShipSize> shipsR6 = new ArrayList<>();
-        RoundCard roundCard6 = new RoundCard();
-        roundCard6.setGameId(gameId);
-        roundCard6.setHeads(RoundCardType.TWO_HEADS);
-        shipsR6.add(ShipSize.L);
-        shipsR6.add(ShipSize.L);
-        shipsR6.add(ShipSize.M);
-        shipsR6.add(ShipSize.S);
-        roundCard6.setShipSizes(shipsR6);
-        roundCardRepository.save(roundCard6);
-
-        ArrayList<ShipSize> shipsR7 = new ArrayList<>();
-        RoundCard roundCard7 = new RoundCard();
-        roundCard7.setGameId(gameId);
-        roundCard7.setHeads(RoundCardType.TWO_HEADS);
-        shipsR7.add(ShipSize.XL);
-        shipsR7.add(ShipSize.L);
-        shipsR7.add(ShipSize.M);
-        shipsR7.add(ShipSize.S);
-        roundCard7.setShipSizes(shipsR7);
-        roundCardRepository.save(roundCard7);
-
-    };
-    public void createSevenThreeHeadCards(){};
-    public void createSevenFourHeadCards(){};
+        roundCardRepository.save(roundCard);
+    }
 
     /**
      * Deals a random card from the currently available roundcards.
