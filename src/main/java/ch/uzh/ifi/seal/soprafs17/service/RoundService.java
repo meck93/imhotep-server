e42deacbc9a081d05d460780c9d2af52623670de
meck93 <moritz.eck@gmail.com>
2017-03-31 00:00:44 +0200

    Changed List Type of currentShips in RoundService

--- src/main/java/ch/uzh/ifi/seal/soprafs17/service/RoundService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs17/service/RoundService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -47,16 +48,15 @@ public class RoundService {
 
             // getting a new roundCard
             RoundCard newRoundCard = roundCardService.getRoundCard(gameId);
-            //delete the dealt card
-            //roundCardService.deleteCard(newRoundCard);
 
+            // Creating a new Round
             Round newRound = new Round();
             newRound.setGame(game);
             newRound.setCard(newRoundCard);
             newRound.setMoves(new ArrayList<>());
 
-            // adding ships to a round
-            ArrayList<Ship> currentShips = shipService.createShips(newRoundCard);
+            // adding ships to the round
+            List<Ship> currentShips = shipService.createShips(newRoundCard);
             newRound.setShips(currentShips);
             roundRepository.save(newRound);
 
