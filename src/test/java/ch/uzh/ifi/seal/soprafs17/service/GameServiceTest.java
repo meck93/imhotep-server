df9bdc62e9ae7faacc184200e97227d4b8455b59
Cristian De Iaco <deiaco.cristian@gmail.com>
2017-04-06 22:44:43 +0200

    added @DirtiesContext to GameServiceTest to cleanup after every test case

--- src/test/java/ch/uzh/ifi/seal/soprafs17/service/GameServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs17/service/GameServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @see GameService
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringApplicationConfiguration(classes = Application.class)
 @Transactional
 public class GameServiceTest {
