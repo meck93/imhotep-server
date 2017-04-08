438f86a2dd2ccf72c376d2a39bf34e958bd315b2
meck93 <moritz.eck@gmail.com>
2017-04-09 01:15:39 +0200

    Changed order of static and final in GameConstant class

--- src/main/java/ch/uzh/ifi/seal/soprafs17/GameConstants.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs17/GameConstants.java
@@ -3,30 +3,30 @@ package ch.uzh.ifi.seal.soprafs17;
 public class GameConstants {
 
 	// max and min players per game
-	public final static Integer MIN_PLAYERS = 2;
-	public final static Integer MAX_PLAYERS = 4;
+	public static final Integer MIN_PLAYERS = 2;
+	public static final Integer MAX_PLAYERS = 4;
 
 	// color for the players
-	public final static String BROWN = "BROWN";
-	public final static String GRAY = "GRAY";
-	public final static String WHITE = "WHITE";
-	public final static String BLACK = "BLACK";
+	public static final String BROWN = "BROWN";
+	public static final String GRAY = "GRAY";
+	public static final String WHITE = "WHITE";
+	public static final String BLACK = "BLACK";
 
 	// different types of moves / discriminator values for hibernate db
-	public final static String GET_STONES = "GET_STONES";
-	public final static String PLACE_STONE = "PLACE_STONE";
-	public final static String SAIL_SHIP = "SAIL_SHIP";
-	public final static String PLAY_CARD = "PLAY_CARD";
+	public static final String GET_STONES = "GET_STONES";
+	public static final String PLACE_STONE = "PLACE_STONE";
+	public static final String SAIL_SHIP = "SAIL_SHIP";
+	public static final String PLAY_CARD = "PLAY_CARD";
 
 	// starting points
-	public final static int START_POINTS = 0;
+	public static final int START_POINTS = 0;
 
 	// amount of starting stones
-	public final static int START_STONES = 30;
+	public static final int START_STONES = 30;
 
 	// max stones on StoneQuarry
-	public final static int MAX_STONES_SUPPLY_SLED = 5;
-	public final static int MAX_STONES_ADDED_PER_MOVE = 3;
+	public static final int MAX_STONES_SUPPLY_SLED = 5;
+	public static final int MAX_STONES_ADDED_PER_MOVE = 3;
 
 	// sonarquabe suggested to add a private constructor instead of the implicit public one
 	private GameConstants() {};
