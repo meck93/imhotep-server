f59a1c07f1036941d67284443f887ef06b4dc51f
meck93 <moritz.eck@gmail.com>
2017-04-12 22:13:10 +0200

    Added Swagger UI + Documentation

--- src/main/java/ch/uzh/ifi/seal/soprafs17/Application.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs17/Application.java
@@ -7,9 +7,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @ComponentScan
 @EnableAutoConfiguration
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
@@ -24,5 +30,14 @@ public class Application {
                 registry.addMapping("/**").allowedOrigins("*");
             }
         };
+    }
+
+    @Bean
+    public Docket getApi(){
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+
     }
 }
