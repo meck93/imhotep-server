35b40bcc6982a35a4495e92e7166a4dce866d001
meck93 <moritz.eck@gmail.com>
2017-04-14 10:15:02 +0200

    Readded Swagger2 - commented it out in Application.class

--- src/main/java/ch/uzh/ifi/seal/soprafs17/Application.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs17/Application.java
@@ -2,15 +2,40 @@ package ch.uzh.ifi.seal.soprafs17;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan
 @EnableAutoConfiguration
+//@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+    // If you want to use Swagger remove those comments
+
+/*    @Bean
+    public Docket getApi(){
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+
+    }*/
 
 }
