4eed530dad5a1f633781860f98d915fe04f60b84
meck93 <moritz.eck@gmail.com>
2017-04-15 12:22:09 +0200

    #42: Implemented checkNextRound + checkSubRound

--- src/main/java/ch/uzh/ifi/seal/soprafs17/service/move/MoveService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs17/service/move/MoveService.java
@@ -1,5 +1,6 @@
 package ch.uzh.ifi.seal.soprafs17.service.move;
 
+import ch.uzh.ifi.seal.soprafs17.GameConstants;
 import ch.uzh.ifi.seal.soprafs17.constant.GameStatus;
 import ch.uzh.ifi.seal.soprafs17.entity.game.Game;
 import ch.uzh.ifi.seal.soprafs17.entity.move.AMove;
@@ -8,6 +9,8 @@ import ch.uzh.ifi.seal.soprafs17.exceptions.InternalServerException;
 import ch.uzh.ifi.seal.soprafs17.exceptions.MoveValidationException;
 import ch.uzh.ifi.seal.soprafs17.exceptions.http.BadRequestHttpException;
 import ch.uzh.ifi.seal.soprafs17.repository.GameRepository;
+import ch.uzh.ifi.seal.soprafs17.service.GameService;
+import ch.uzh.ifi.seal.soprafs17.service.game.RoundService;
 import ch.uzh.ifi.seal.soprafs17.service.move.rule.RuleManager;
 import ch.uzh.ifi.seal.soprafs17.service.move.validation.ValidationManager;
 import org.slf4j.Logger;
@@ -28,12 +31,16 @@ public class MoveService {
     private final GameRepository gameRepository;
     private final ValidationManager validationManager;
     private final RuleManager ruleManager;
+    private final GameService gameService;
+    private final RoundService roundService;
 
     @Autowired
-    public MoveService(GameRepository gameRepository, ValidationManager validationManager, RuleManager ruleManager) {
+    public MoveService(GameRepository gameRepository, ValidationManager validationManager, RuleManager ruleManager, GameService gameService, RoundService roundService) {
         this.gameRepository = gameRepository;
         this.validationManager = validationManager;
         this.ruleManager = ruleManager;
+        this.gameService = gameService;
+        this.roundService = roundService;
     }
 
     public synchronized void validateAndApply(AMove move) throws BadRequestHttpException, InternalServerException {
@@ -57,17 +64,40 @@ public class MoveService {
             throw new InternalServerException(applyMoveException);
         }
 
+        // Saving the changed Game state into the DB
+        this.gameRepository.save(game);
+
+        // Checking if the game advances to the next Round
+        this.checkSubRound(move, game);
+        this.checkNextRound(move, game);
+    }
+
+    public synchronized void checkNextRound(AMove move, Game game){
         // Advancing the Game to the next Player, only if the Game is in Status: RUNNING
         if ((game.getStatus() == GameStatus.RUNNING)) {
+            // Advancing the currentPlayer
             game.setCurrentPlayer((game.getCurrentPlayer()) % (game.getPlayers().size()) + 1);
+
+            // Checking whether all ships have been sailed or not
+            if (move.getMoveType().equals(GameConstants.SAIL_SHIP) && this.roundService.goToNextRound(game.getRoundByRoundCounter(game.getRoundCounter()))){
+                // After six Rounds the Game will be ended
+                if (game.getRoundCounter() == GameConstants.LAST_ROUND) {
+                    this.gameService.stopGame(game.getId());
+                }
+                // Game is not finished yet
+                else {
+                    // All Ships have sailed -> Initialize a new Round
+                    this.gameService.initializeRound(game.getId());
+                }
+            }
         }
+    }
+
+    public synchronized void checkSubRound(AMove move, Game game){
         // Advancing the Sub-Round when the Game is in Status: SUBROUND
         if (game.getStatus() == GameStatus.SUBROUND){
             // Advancing the currentSubRoundPlayer to the next Player according to the Stone on the Ship
             // game.setCurrentSubRoundPlayer();
         }
-
-        // Saving the changed Game state into the DB
-        this.gameRepository.save(game);
     }
 }
