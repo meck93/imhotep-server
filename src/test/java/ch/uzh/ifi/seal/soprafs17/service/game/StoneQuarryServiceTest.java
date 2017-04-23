aed6b4882c794afded405f35c83efd9bab7c0429
Cristian De Iaco <deiaco.cristian@gmail.com>
2017-04-23 21:46:30 +0200

    Tested StoneQuarryService

--- src/test/java/ch/uzh/ifi/seal/soprafs17/service/game/StoneQuarryServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs17/service/game/StoneQuarryServiceTest.java
@@ -1,8 +1,17 @@
 package ch.uzh.ifi.seal.soprafs17.service.game;
 
 import ch.uzh.ifi.seal.soprafs17.Application;
+import ch.uzh.ifi.seal.soprafs17.GameConstants;
+import ch.uzh.ifi.seal.soprafs17.entity.game.Game;
+import ch.uzh.ifi.seal.soprafs17.entity.game.Stone;
+import ch.uzh.ifi.seal.soprafs17.entity.game.StoneQuarry;
+import ch.uzh.ifi.seal.soprafs17.entity.user.Player;
+import ch.uzh.ifi.seal.soprafs17.exceptions.InternalServerException;
+import ch.uzh.ifi.seal.soprafs17.exceptions.http.NotFoundException;
 import ch.uzh.ifi.seal.soprafs17.repository.StoneQuarryRepository;
-import ch.uzh.ifi.seal.soprafs17.service.game.StoneQuarryService;
+import ch.uzh.ifi.seal.soprafs17.service.GameService;
+import ch.uzh.ifi.seal.soprafs17.service.user.UserService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +19,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -24,18 +36,86 @@ import org.springframework.transaction.annotation.Transactional;
 public class StoneQuarryServiceTest {
 
     @Autowired
-    private StoneQuarryService stoneQuarryService;
+    public StoneQuarryService stoneQuarryService;
 
     @Autowired
-    private StoneQuarryRepository stoneQuarryRepository;
+    public StoneQuarryRepository stoneQuarryRepository;
+
+    @Autowired
+    public GameService gameService;
+
+    @Autowired
+    public UserService userService;
 
     @Test
     public void createStoneQuarry() {
-        // TODO: test stoneQuarryService.createStoneQuarry()
+        Game testGame = gameService.createGame("testGame", "testOwner1");
+        testGame.setNumberOfPlayers(2);
+        Assert.assertNotNull(stoneQuarryService.createStoneQuarry(testGame));
+        Assert.assertNotNull(stoneQuarryRepository.findOne(1L).getBlackStones());
+        Assert.assertNotNull(stoneQuarryRepository.findOne(1L).getWhiteStones());
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        List<Player> testPlayers = new ArrayList<>();
+        testPlayers.add(player1);
+        testPlayers.add(player2);
+        testPlayers.add(player3);
+        testGame.setPlayers(testPlayers);
+        Assert.assertNotNull(stoneQuarryService.createStoneQuarry(testGame));
+        Assert.assertNotNull(stoneQuarryRepository.findOne(2L).getBrownStones());
+        Player player4 = new Player();
+        testPlayers.add(player4);
+        testGame.setPlayers(testPlayers);
+        Assert.assertNotNull(stoneQuarryService.createStoneQuarry(testGame));
+        Assert.assertNotNull(stoneQuarryRepository.findOne(3L).getGrayStones());
     }
 
     @Test
     public void fillQuarry() {
-        // TODO: test stoneQuarryService.fillQuarry()
+        Game testGame = gameService.createGame("testGame", "testOwner1");
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
+        List<Player> testPlayers = new ArrayList<>();
+        testPlayers.add(player1);
+        testPlayers.add(player2);
+        testPlayers.add(player3);
+        testPlayers.add(player4);
+        testGame.setPlayers(testPlayers);
+        StoneQuarry stoneQuarry1 = stoneQuarryService.createStoneQuarry(testGame);
+        stoneQuarryService.fillQuarry(stoneQuarry1);
+        Assert.assertEquals(stoneQuarryRepository.findOne(1L).getBlackStones().size(), GameConstants.START_STONES);
+        Assert.assertEquals(stoneQuarryRepository.findOne(1L).getWhiteStones().size(), GameConstants.START_STONES);
+        Assert.assertEquals(stoneQuarryRepository.findOne(1L).getBrownStones().size(), GameConstants.START_STONES);
+        Assert.assertEquals(stoneQuarryRepository.findOne(1L).getGrayStones().size(), GameConstants.START_STONES);
+    }
+
+    @Test
+    public void getStone() {
+        Game testGame = gameService.createGame("testGame", "testOwner1");
+        StoneQuarry testQuarry = stoneQuarryService.createStoneQuarry(testGame);
+        try{
+            stoneQuarryService.getStone(1L,1);
+        } catch(InternalServerException e) {}
+        try{
+            List<Stone> testStones = new ArrayList<>();
+            testQuarry.setBlackStones(testStones);
+        } catch(InternalServerException e) {}
+        stoneQuarryService.fillQuarry(testQuarry);
+        Assert.assertNotNull(stoneQuarryService.getStone(1L,1));
+    }
+
+    @Test
+    public void getStoneQuarryByGameId() {
+        Game testGame = gameService.createGame("testGame", "testOwner1");
+        try{
+            stoneQuarryService.getStoneQuarryByGameId(1L);
+        } catch(NotFoundException e) {}
+
+        testGame.setNumberOfPlayers(2);
+        StoneQuarry testQuarry = stoneQuarryService.createStoneQuarry(testGame);
+        Assert.assertEquals(stoneQuarryService.getStoneQuarryByGameId(1L),testQuarry);
     }
 }
