012e08a8b883f980d62f11f757192c661263a9a9
Cristian De Iaco <deiaco.cristian@gmail.com>
2017-04-24 18:33:32 +0200

    Tested the ShipService

--- src/test/java/ch/uzh/ifi/seal/soprafs17/service/game/ShipServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs17/service/game/ShipServiceTest.java
@@ -1,8 +1,12 @@
 package ch.uzh.ifi.seal.soprafs17.service.game;
 
 import ch.uzh.ifi.seal.soprafs17.Application;
+import ch.uzh.ifi.seal.soprafs17.constant.ShipSize;
+import ch.uzh.ifi.seal.soprafs17.entity.card.RoundCard;
+import ch.uzh.ifi.seal.soprafs17.entity.game.Game;
+import ch.uzh.ifi.seal.soprafs17.entity.game.Ship;
 import ch.uzh.ifi.seal.soprafs17.repository.ShipRepository;
-import ch.uzh.ifi.seal.soprafs17.service.game.ShipService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -31,11 +38,39 @@ public class ShipServiceTest {
 
     @Test
     public void createShips() {
-        // TODO: test shipService.createShips()
+        Game testGame = new Game();
+        testGame.setId(1L);
+        RoundCard testCard = new RoundCard();
+        List<ShipSize> testShipSizes = new ArrayList<>();
+        testShipSizes.add(ShipSize.XL);
+        testShipSizes.add(ShipSize.L);
+        testShipSizes.add(ShipSize.M);
+        testShipSizes.add(ShipSize.S);
+        testCard.setShipSizes(testShipSizes);
+        List<Ship> testShips = shipService.createShips(testCard);
+        Assert.assertNotNull(testShips);
     }
 
     @Test
     public void createShip() {
-        // TODO: test shipService.createShip()
+        Game testGame = new Game();
+        testGame.setId(1L);
+        shipService.createShip(4,2, 1L);
+        shipService.createShip(3,2, 1L);
+        shipService.createShip(2,1, 1L);
+        shipService.createShip(1,1, 1L);
+        Assert.assertNotNull(shipRepository.findOne(1L));
+        Assert.assertNotNull(shipRepository.findOne(2L));
+        Assert.assertNotNull(shipRepository.findOne(3L));
+        Assert.assertNotNull(shipRepository.findOne(4L));
+    }
+
+    @Test
+    public void findShip() {
+        Game testGame = new Game();
+        testGame.setId(1L);
+        shipService.createShip(4,2, 1L);
+        Assert.assertNotNull(shipService.findShip(1L));
+
     }
 }
