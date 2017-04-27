651371ca25e5e0e4362cfa9758a2b46f564f24ed
Cristian De Iaco <deiaco.cristian@gmail.com>
2017-04-27 17:19:49 +0200

    Added tests in LobbyServiceTest

--- src/test/java/ch/uzh/ifi/seal/soprafs17/service/game/LobbyServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs17/service/game/LobbyServiceTest.java
@@ -1,8 +1,14 @@
 package ch.uzh.ifi.seal.soprafs17.service.game;
 
 import ch.uzh.ifi.seal.soprafs17.Application;
+import ch.uzh.ifi.seal.soprafs17.constant.GameStatus;
 import ch.uzh.ifi.seal.soprafs17.entity.game.Game;
+import ch.uzh.ifi.seal.soprafs17.entity.user.Player;
+import ch.uzh.ifi.seal.soprafs17.entity.user.User;
+import ch.uzh.ifi.seal.soprafs17.exceptions.http.BadRequestHttpException;
+import ch.uzh.ifi.seal.soprafs17.exceptions.http.NotFoundException;
 import ch.uzh.ifi.seal.soprafs17.repository.GameRepository;
+import ch.uzh.ifi.seal.soprafs17.repository.StoneQuarryRepository;
 import ch.uzh.ifi.seal.soprafs17.service.GameService;
 import ch.uzh.ifi.seal.soprafs17.service.user.PlayerService;
 import ch.uzh.ifi.seal.soprafs17.service.user.UserService;
@@ -45,6 +51,10 @@ public class LobbyServiceTest {
     @Autowired
     public UserService userService;
 
+    @Autowired
+    public StoneQuarryRepository stoneQuarryRepository;
+
+
     @Test
     public void listGames() {
         Assert.assertNull(gameRepository.findById(1L));
@@ -59,41 +69,166 @@ public class LobbyServiceTest {
 
     @Test
     public void createGame() {
-        // TODO: test lobbyService.createGame()
+        User user1 = userService.createUser("testName","testOwner");
+        Game game = gameService.createGame("testGame","testOwner");
+        Assert.assertNotNull(game);
+        try{
+            lobbyService.createGame(game,user1.getId());
+            Assert.assertNotNull(game);
+        } catch (BadRequestHttpException e) {}
     }
 
     @Test
     public void joinGame() {
-        // TODO: test lobbyService.joinGame()
+        User user1 = userService.createUser("testName","testOwner");
+        User user2 = userService.createUser("testName2","testOwner2");
+        Game game = gameService.createGame("testGame","testOwner");
+        lobbyService.joinGame(1L,2L);
+        lobbyService.joinGame(1L,1L);
+        Assert.assertEquals(game.getNumberOfPlayers(),2);
     }
 
     @Test
+    public void removePlayer() {
+        Game game = gameService.createGame("testGame", "testOwner1");
+
+        User user1 = userService.createUser("testUser1", "testOwner1");
+        User user2 = userService.createUser("testUser2", "testOwner2");
+
+        Player player1 = playerService.createPlayer(game.getId(), user1.getId());
+        playerService.initializePlayer(game.getId(), player1);
+        gameService.addPlayer(game.getId(), player1);
+        gameService.updateNrOfPlayers(game.getId());
+
+        Player player2 = playerService.createPlayer(game.getId(), user2.getId());
+        playerService.initializePlayer(game.getId(), player2);
+        gameService.addPlayer(game.getId(), player2);
+        gameService.updateNrOfPlayers(game.getId());
+
+        lobbyService.removePlayer(1L,1L);
+        try{
+            Assert.assertNull(playerService.findPlayerById(2L));
+        } catch (NotFoundException e) {}
+    }
+
+
+    @Test
     public void startGame() {
-        /*
-        Game game = gameService.createGame("test","testOwner");
-        game.setId(1L);
-        User user1 = userService.createUser("testName","testOwner");
-        user1.setId(1L);
-        Player player1 = playerService.createPlayer(1L,1L);
-        player1.setPlayerId(1L);
-        lobbyService.joinGame(1L,1L);
-        try {
-            lobbyService.startGame(1L,1L);
-            Assert.assertEquals(GameStatus.RUNNING, game.getStatus());
-        } catch(BadRequestHttpException e){}
+        User user1 = userService.createUser("testUser1", "test1");
+        User user2 = userService.createUser("testUser2", "test2");
 
-        User user2 = userService.createUser("testName2","testOwner2");
-        user2.setId(2L);
-        Player player2 = playerService.createPlayer(1L,2L);
-        player2.setPlayerId(2L);
-        lobbyService.joinGame(1L,2L);
-        try {
-            lobbyService.startGame(1L,2L);
-            Assert.assertEquals(GameStatus.RUNNING, game.getStatus());
-        } catch(BadRequestHttpException e){}
+        Game game = gameService.createGame("testGame", "test1");
 
-        //lobbyService.startGame(1L,1L);
-        //Assert.assertEquals(GameStatus.RUNNING, game.getStatus());
-        */
+        Player player1 = playerService.createPlayer(game.getId(), user1.getId());
+        playerService.initializePlayer(game.getId(), player1);
+        gameService.addPlayer(game.getId(), player1);
+        gameService.updateNrOfPlayers(game.getId());
+
+        Player player2 = playerService.createPlayer(game.getId(), user2.getId());
+        playerService.initializePlayer(game.getId(), player2);
+        gameService.addPlayer(game.getId(), player2);
+        gameService.updateNrOfPlayers(game.getId());
+
+        Assert.assertEquals(game.getStatus(), GameStatus.PENDING);
+        lobbyService.startGame(game.getId(),player1.getId());
+        Assert.assertEquals(game.getStatus(), GameStatus.RUNNING);
+    }
+
+    @Test
+    public void stopGame() {
+        User user1 = userService.createUser("testUser1", "test1");
+        User user2 = userService.createUser("testUser2", "test2");
+
+        Game game = gameService.createGame("testGame", "test1");
+
+        Player player1 = playerService.createPlayer(game.getId(), user1.getId());
+        playerService.initializePlayer(game.getId(), player1);
+        gameService.addPlayer(game.getId(), player1);
+        gameService.updateNrOfPlayers(game.getId());
+
+        Player player2 = playerService.createPlayer(game.getId(), user2.getId());
+        playerService.initializePlayer(game.getId(), player2);
+        gameService.addPlayer(game.getId(), player2);
+        gameService.updateNrOfPlayers(game.getId());
+
+        lobbyService.startGame(game.getId(),player1.getId());
+        lobbyService.stopGame(1L);
+        Assert.assertEquals(game.getStatus(),GameStatus.FINISHED);
+    }
+
+    @Test
+    public void deleteGame() {
+        User user1 = userService.createUser("testUser1", "test1");
+        User user2 = userService.createUser("testUser2", "test2");
+
+        Game game = gameService.createGame("testGame", "test1");
+
+        Player player1 = playerService.createPlayer(game.getId(), user1.getId());
+        playerService.initializePlayer(game.getId(), player1);
+        gameService.addPlayer(game.getId(), player1);
+        gameService.updateNrOfPlayers(game.getId());
+
+        Player player2 = playerService.createPlayer(game.getId(), user2.getId());
+        playerService.initializePlayer(game.getId(), player2);
+        gameService.addPlayer(game.getId(), player2);
+        gameService.updateNrOfPlayers(game.getId());
+
+        lobbyService.startGame(game.getId(),player1.getId());
+        lobbyService.deleteGame(game.getId());
+
+        Assert.assertEquals(gameRepository.findPlayersByGameId(1L).size(),0);
+    }
+
+    @Test
+    public void fastForward() {
+        User user1 = userService.createUser("testUser1", "test1");
+        User user2 = userService.createUser("testUser2", "test2");
+
+        Game game = gameService.createGame("testGame", "test1");
+
+        Player player1 = playerService.createPlayer(game.getId(), user1.getId());
+        playerService.initializePlayer(game.getId(), player1);
+        gameService.addPlayer(game.getId(), player1);
+        gameService.updateNrOfPlayers(game.getId());
+
+        Player player2 = playerService.createPlayer(game.getId(), user2.getId());
+        playerService.initializePlayer(game.getId(), player2);
+        gameService.addPlayer(game.getId(), player2);
+        gameService.updateNrOfPlayers(game.getId());
+
+        try{
+            lobbyService.startGame(game.getId(),player1.getId());
+            lobbyService.fastForward(game.getId(),player1.getId());
+        } catch (BadRequestHttpException e) {}
+
+        User user3 = userService.createUser("testUser3", "test3");
+        User user4 = userService.createUser("testUser4", "test4");
+
+        Game game2 = gameService.createGame("testGame2", "test3");
+
+        Player player3 = playerService.createPlayer(game2.getId(), user3.getId());
+        playerService.initializePlayer(game2.getId(), player3);
+        gameService.addPlayer(game2.getId(), player3);
+        gameService.updateNrOfPlayers(game2.getId());
+
+        // Can't start if there aren't at least 2 players in a game
+        try{
+            lobbyService.fastForward(game2.getId(),player3.getId());
+        } catch (BadRequestHttpException e) {}
+
+        // Can't start the start if player is not the owner
+        try{
+            lobbyService.fastForward(game2.getId(),player2.getId());
+        } catch (BadRequestHttpException e){}
+
+        Player player4 = playerService.createPlayer(game2.getId(), user4.getId());
+        playerService.initializePlayer(game2.getId(), player4);
+        gameService.addPlayer(game2.getId(), player4);
+        gameService.updateNrOfPlayers(game2.getId());
+
+        game2.setStoneQuarry(this.stoneQuarryRepository.findOne(1L));
+
+        //lobbyService.fastForward(game2.getId(),player3.getId());
+
     }
 }
