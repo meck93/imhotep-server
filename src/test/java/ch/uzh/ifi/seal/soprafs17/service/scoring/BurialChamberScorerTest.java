0b8d6a2cd63adf7a9eed9850afed36c78d6b8f82
dolcem <deiaco.cristian@gmail.com>
2017-05-11 14:08:19 +0200

    Commented BurialChamberScorer and BurialChamberScorerTest

--- src/test/java/ch/uzh/ifi/seal/soprafs17/service/scoring/BurialChamberScorerTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs17/service/scoring/BurialChamberScorerTest.java
@@ -56,6 +56,7 @@ public class BurialChamberScorerTest {
     @Test
     public void scoreNow() {
         try {
+            // score the chamber now. (Should return null, since the chamber gets scored at the end of the game)
             Assert.assertNull(burialChamberScorer.scoreNow(new Game()));
         } catch (NullPointerException e) {}
     }
@@ -63,13 +64,16 @@ public class BurialChamberScorerTest {
     @Test
     public void scoreEndOfRound() {
         try {
+            // score the chamber at the end of the round. (Should return null, since the chamber gets scored at the end of the game)
             Assert.assertNull(burialChamberScorer.scoreEndOfRound(new Game()));
         } catch (NullPointerException e) {}
     }
 
     @Test
     public void convertToArray(){
+        // Create List of stones
         List<Stone> stones = new ArrayList<>();
+        // Create stones
         Stone stone1 = new Stone();
         Stone stone2 = new Stone();
         Stone stone3 = new Stone();
@@ -80,13 +84,14 @@ public class BurialChamberScorerTest {
         Stone stone8 = new Stone();
 
 
-
+        // testArray: Player 1's stones = 1, Player 2's stones = 2, Player 3's stones = 33, Player 4's stones = 4,
         int[] testArray ={
                 1,4,3,0,0,0,0,0,0,0,
                 2,1,4,0,0,0,0,0,0,0,
                 3,2,0,0,0,0,0,0,0,0
         };
 
+        // Set the stone's color according to the testArray
         stone1.setColor(GameConstants.BLACK);
         stone2.setColor(GameConstants.WHITE);
         stone3.setColor(GameConstants.BROWN);
@@ -96,6 +101,7 @@ public class BurialChamberScorerTest {
         stone7.setColor(GameConstants.BROWN);
         stone8.setColor(GameConstants.GRAY);
 
+        // Add the stones to the list
         stones.add(stone1);
         stones.add(stone2);
         stones.add(stone3);
@@ -106,31 +112,44 @@ public class BurialChamberScorerTest {
         stones.add(stone7);
         stones.add(stone8);
 
+        // Convert the list to an array to compute the algorithm
         int[] resultArray = burialChamberScorer.convertToArray(stones);
 
+        // First stone is a black stone (player 1)
         Assert.assertEquals(resultArray[0],1);
+        // Second stone is a white stone (player 2)
         Assert.assertEquals(resultArray[10],2);
+        // Third stone is a brown stone (player 3)
         Assert.assertEquals(resultArray[20],3);
+        // Fourth stone is a gray stone (player 4)
         Assert.assertEquals(resultArray[1],4);
 
         for (int i = 0; i < resultArray.length;i++) {
+            // check if the list was correctly converted
             Assert.assertEquals(resultArray[i], testArray[i]);
         }
     }
 
     @Test
     public void isValid(){
+        // Test index 0
         int pos1 = 0;
+        // Test a negative index
         int pos2 = -360;
+        // Test a index not contained in our array
         int pos3 = 420;
 
+        // 0 is a valid index
         Assert.assertEquals(burialChamberScorer.isValid(pos1),true);
+        // -360 is an invalid index
         Assert.assertEquals(burialChamberScorer.isValid(pos2),false);
+        // 420 is an invalid index
         Assert.assertEquals(burialChamberScorer.isValid(pos3),false);
     }
 
     @Test
     public void lookDown(){
+        // testArray
         int[] testArray ={
                 1,2,3,4,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0,
@@ -142,14 +161,19 @@ public class BurialChamberScorerTest {
         int pos3 = 420;
         int pos4 = -360;
 
+        // Valid lookDown
         Assert.assertEquals(burialChamberScorer.lookDown(testArray,pos1),10);
+        // Invalid lookDown
         Assert.assertEquals(burialChamberScorer.lookDown(testArray,pos2),-1);
+        // Invalid lookDown
         Assert.assertEquals(burialChamberScorer.lookDown(testArray,pos3),-1);
+        // Invalid lookDown
         Assert.assertEquals(burialChamberScorer.lookDown(testArray,pos4),-1);
     }
 
     @Test
     public void lookUp(){
+        // testArray
         int[] testArray ={
                 1,2,3,4,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0,
@@ -161,14 +185,19 @@ public class BurialChamberScorerTest {
         int pos3 = 420;
         int pos4 = -360;
 
+        // Invalid lookUp
         Assert.assertEquals(burialChamberScorer.lookUp(testArray,pos1),-1);
+        // Valid lookUp
         Assert.assertEquals(burialChamberScorer.lookUp(testArray,pos2),13);
+        // Invalid lookUp
         Assert.assertEquals(burialChamberScorer.lookUp(testArray,pos3),-1);
+        // Invalid lookUp
         Assert.assertEquals(burialChamberScorer.lookUp(testArray,pos4),-1);
     }
 
     @Test
     public void lookRight(){
+        // testArray
         int[] testArray ={
                 1,2,3,4,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0,
@@ -180,14 +209,19 @@ public class BurialChamberScorerTest {
         int pos3 = 420;
         int pos4 = -360;
 
+        // Valid lookRight
         Assert.assertEquals(burialChamberScorer.lookRight(testArray,pos1),1);
+        // Invalid lookRight
         Assert.assertEquals(burialChamberScorer.lookRight(testArray,pos2),-1);
+        // Invalid lookRight
         Assert.assertEquals(burialChamberScorer.lookRight(testArray,pos3),-1);
+        // Invalid lookRight
         Assert.assertEquals(burialChamberScorer.lookRight(testArray,pos4),-1);
     }
 
     @Test
     public void lookLeft(){
+        // testArray
         int[] testArray ={
                 1,2,3,4,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0,
@@ -199,100 +233,116 @@ public class BurialChamberScorerTest {
         int pos3 = 420;
         int pos4 = -360;
 
+        // Invalid lookLeft
         Assert.assertEquals(burialChamberScorer.lookLeft(testArray,pos1),-1);
+        // Valid lookLeft
         Assert.assertEquals(burialChamberScorer.lookLeft(testArray,pos2),8);
+        // Invalid lookLeft
         Assert.assertEquals(burialChamberScorer.lookLeft(testArray,pos3),-1);
+        // Invalid lookLeft
         Assert.assertEquals(burialChamberScorer.lookLeft(testArray,pos4),-1);
     }
 
     @Test
     public void incrementCount() {
+        // test counters
         int counter1 = 0;
         int counter2 = 420;
         int counter3 = -360;
         int counter4 = 20;
 
+        // testArray
         int[] testArray ={
                 1,2,3,4,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0,
                 0,0,0,0,0,0,0,0,0,0
         };
 
+        // Valid lookRight
         Assert.assertEquals(burialChamberScorer.incrementCount(counter1,testArray),10);
+        // Invalid lookRight
         Assert.assertEquals(burialChamberScorer.incrementCount(counter2,testArray),-1);
+        // Invalid lookRight
         Assert.assertEquals(burialChamberScorer.incrementCount(counter3,testArray),-1);
+        // Valid lookRight
         Assert.assertEquals(burialChamberScorer.incrementCount(counter4,testArray),1);
     }
 
     @Test
     public void findLookUpIndex(){
-
+        // testArray
         int[] testArray ={
                 1,-2,3,4,0,0,0,0,0,0,
                 0,0,1,0,0,0,3,0,0,0,
                 0,0,1,0,0,0,4,0,0,0
         };
 
+        // find lookUpIndex
         Assert.assertEquals(burialChamberScorer.findLookUpIndex(testArray),1);
 
     }
 
     @Test
     public void addPoints(){
+        // Create game and user
         Game game = gameService.createGame("testName", "testOwner");
         User user1 = userService.createUser("test1");
+        // Convert user to player
         Player player1 = playerService.createPlayer(1L,1L);
         user1.setPlayer(player1);
+        // Add player to game
         List<Player> players = new ArrayList<>();
         players.add(player1);
         game.setPlayers(players);
-
+        // Initialize player points
         int[] points = {0,0,0,0};
         player1.setPoints(points);
-
+        // score points
         burialChamberScorer.addPoints(game,1,0);
         Assert.assertEquals(player1.getPoints()[3],0);
-
+        // score points
         burialChamberScorer.addPoints(game,1,1);
         Assert.assertEquals(player1.getPoints()[3],1);
-
+        // score points
         burialChamberScorer.addPoints(game,1,2);
         Assert.assertEquals(player1.getPoints()[3],4);
-
+        // score points
         burialChamberScorer.addPoints(game,1,3);
         Assert.assertEquals(player1.getPoints()[3],10);
-
+        // score points
         burialChamberScorer.addPoints(game,1,4);
         Assert.assertEquals(player1.getPoints()[3],20);
-
+        // score points
         burialChamberScorer.addPoints(game,1,5);
         Assert.assertEquals(player1.getPoints()[3],35);
-
+        // score points
         burialChamberScorer.addPoints(game,1,6);
         Assert.assertEquals(player1.getPoints()[3],52);
     }
 
     @Test
     public void scoreChamber(){
-
+        // testArray
         int[] testArray ={
                 0,1,1,1,1,0,0,0,0,0,
                 1,0,1,0,1,0,0,0,0,0,
                 0,1,1,1,0,0,0,0,0,0
         };
 
-
+        // create game and user
         Game game = gameService.createGame("testName", "testOwner");
         User user1 = userService.createUser("test1");
+        // convert user to player
         Player player1 = playerService.createPlayer(1L,1L);
         user1.setPlayer(player1);
+        // add player to the game
         List<Player> players = new ArrayList<>();
         players.add(player1);
         game.setPlayers(players);
-
+        // initialize player score
         int[] points = {0,0,0,0};
         player1.setPoints(points);
-
+        // score chamber
         burialChamberScorer.scoreChamber(testArray,1,game);
         Assert.assertEquals(player1.getPoints()[3],24);
     }
